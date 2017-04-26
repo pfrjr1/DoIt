7bcad488e331769aa685fc12b96b9ce3fb1127a9
Peter Rooney <pfrjr1@me.com>
2017-04-26 15:15:58 -0400

    5mins in lect 59

---
+++ DoIt/Task.swift
@@ -0,0 +1,16 @@
+//
+//  Task.swift
+//  DoIt
+//
+//  Created by Peter Rooney on 4/26/17.
+//  Copyright © 2017 Peter Rooney. All rights reserved.
+//
+
+import Foundation
+
+class Task {
+    var name = ""
+    var important = false
+    
+    
+}
